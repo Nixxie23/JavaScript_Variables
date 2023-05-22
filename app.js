afa49680f0785eb9ea04da90cbec6deb6c6c926d
taylor Schuchter <taylorschuchter@gmail.com>
2023-05-22 12:02:27 -0400

    project complete

--- app.js
+++ app.js
@@ -4,7 +4,42 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
-
+const firstName = "Taylor";
+lastName = "Schuchter";
+age = 27;
+console.log(firstName, lastName);
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookMark = 456;
+let hasRead = true;
+console.log(bookMark);
+
+
+
+//Exercise 5: Declaring and Assigning Values to Variables x3
+//Steps are to be completed in the app.js file.
+
+//Declare a variable named title using the let keyword, and assign it the value "Name of the Wind"(string)
+//Declare a variable named author using the let keyword, and assign it the value "Patrick Rothfuss"(string)
+//Declare a variable named pageCount using the let keyword, and assign it the value 722(number)
+//Declare a variable named bookmark using the let keyword, and assign it the value 456(number)
+//Declare a variable named hasRead using the let keyword, and assign it the value true(boolean)
